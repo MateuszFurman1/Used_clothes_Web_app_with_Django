47454f3117b980d115ce978cf0df69cfa443a479
Mati <fur.mateusz@gmail.com>
2023-02-19 17:21:01 +0100

    fixing bug made by pretier

--- Used_clothes_app/management/commands/seed_db.py
+++ Used_clothes_app/management/commands/seed_db.py
@@ -6,24 +6,9 @@ class Command(BaseCommand):
     help = 'Seeds the database with dummy data'
 
     def handle(self, *args, **options):
-        # # Create user instance of the User model using the UserFactory
-        # user = UserFactory.create_batch(1)
-        
-        # # Create 5 instances of the Category model using the CategoryFactory
-        # categories = CategoryFactory.create_batch(5)
-        
-        # # Create 5 instances of the Institution model using the InstitutionFactory
-        # institutions = InstitutionFactory.create_batch(5)
-
-        # # Create 5 instances of the Donation model using the DonationFactory
-        # donations = DonationFactory.create_batch(5)
-
-        # self.stdout.write(self.style.SUCCESS(
-        #     'Successfully seeded the database with dummy data.'))
-        
         with transaction.atomic():
-            users = UserFactory.create_batch(10)
-            categories = CategoryFactory.create_batch(10)
-            institutions = InstitutionFactory.create_batch(10)
-            donations = DonationFactory.create_batch(10)
+            users = UserFactory.create_batch(5)
+            categories = CategoryFactory.create_batch(5)
+            institutions = InstitutionFactory.create_batch(5)
+            donations = DonationFactory.create_batch(5)
 
